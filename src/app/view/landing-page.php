3a1ba07d7c9f5d5f47e336ca1deaea912ad0624b
Alvinjentara <lordjentarr@gmail.com>
2022-12-31 20:24:33 +0700

    landing page hampir

--- src/app/view/landing-page.php
+++ src/app/view/landing-page.php
@@ -13,8 +13,8 @@ use LearnPhpMvc\APP\View;
     <meta name="author" content="TemplateMo">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
-    <title><?=$model['title']?></title>
-<!--
+    <title><?= $model['title'] ?></title>
+    <!--
 
 Lava Landing Page
 
@@ -62,7 +62,7 @@ https://templatemo.com/tm-540-lava-landing-page
                             <li class="scroll-to-section"><a href="#welcome" class="menu-item">Home</a></li>
                             <li class="scroll-to-section"><a href="#about" class="menu-item">About</a></li>
                             <li class="scroll-to-section"><a href="#testimonials" class="menu-item">Testimonials</a>
-                            <li class=""><a href="<?=View::getUrl('admin/login')?>" class="">Log In</a>
+                            <li class=""><a href="<?= View::getUrl('admin/login') ?>" class="">Log In</a>
                             </li>
                             <li class="submenu">
                                 <a href="javascript:;">Drop Down</a>
@@ -71,7 +71,7 @@ https://templatemo.com/tm-540-lava-landing-page
                                     <li><a href="" class="menu-item">Features</a></li>
                                     <li><a href="" class="menu-item">FAQ's</a></li>
                                     <li><a href="" class="menu-item">Blog</a></li>
-                                    <li><a href="<?=View::getUrl('/login')?>" class="menu-item">Log In</a></li>
+                                    <li><a href="<?= View::getUrl('/login') ?>" class="menu-item">Log In</a></li>
                                 </ul>
                             </li>
                             <li class="scroll-to-section"><a href="#contact-us" class="menu-item">Contact Us</a></li>
@@ -95,11 +95,10 @@ https://templatemo.com/tm-540-lava-landing-page
         <div class="header-text">
             <div class="container">
                 <div class="row">
-                    <div class="left-text col-lg-6 col-md-12 col-sm-12 col-xs-12"
-                        data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
-                        <h1>Simple App that we <em>CREATE</em></h1>
+                    <div class="left-text col-lg-6 col-md-12 col-sm-12 col-xs-12" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                        <h1>Build Your <em>BEST BODY</em></h1>
                         <p>Lava <a href="#">HTML landing page</a> template is provided by <a href="#">TemplateMo</a>.
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum natus neque commodi et, error perspiciatis exercitationem minus. Odit accusantium quae error enim? Deserunt dolore dolorem maxime nemo iure eaque?                        <a href="#about" class="main-button-slider">KNOW US BETTER</a>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum natus neque commodi et, error perspiciatis exercitationem minus. Odit accusantium quae error enim? Deserunt dolore dolorem maxime nemo iure eaque?
                     </div>
                 </div>
             </div>
@@ -112,13 +111,12 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum natus neque
     <section class="section" id="about">
         <div class="container">
             <div class="row">
-                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12"
-                    data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
                     <div class="features-item">
                         <div class="features-icon">
                             <h2>01</h2>
                             <img src="plugins/template-landingpage/assets/images/features-icon-1.png" alt="">
-                            <h4>Trend Analysis</h4>
+                            <h4>Strength</h4>
                             <p>Curabitur pulvinar vel odio sed sagittis. Nam maximus ex diam, nec consectetur diam.</p>
                             <a href="#testimonials" class="main-button">
                                 Read More
@@ -126,13 +124,12 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum natus neque
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12"
-                    data-scroll-reveal="enter bottom move 30px over 0.6s after 0.4s">
+                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12" data-scroll-reveal="enter bottom move 30px over 0.6s after 0.4s">
                     <div class="features-item">
                         <div class="features-icon">
                             <h2>02</h2>
                             <img src="plugins/template-landingpage/assets/images/features-icon-2.png" alt="">
-                            <h4>Site Optimization</h4>
+                            <h4>Fitness</h4>
                             <p>Curabitur pulvinar vel odio sed sagittis. Nam maximus ex diam, nec consectetur diam.</p>
                             <a href="#testimonials" class="main-button">
                                 Discover More
@@ -140,13 +137,12 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum natus neque
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12"
-                    data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
+                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12" data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
                     <div class="features-item">
                         <div class="features-icon">
                             <h2>03</h2>
                             <img src="plugins/template-landingpage/assets/images/features-icon-3.png" alt="">
-                            <h4>Email Design</h4>
+                            <h4>Wellness</h4>
                             <p>Curabitur pulvinar vel odio sed sagittis. Nam maximus ex diam, nec consectetur diam.</p>
                             <a href="#testimonials" class="main-button">
                                 More Detail
@@ -165,8 +161,7 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum natus neque
     <section class="section" id="promotion">
         <div class="container">
             <div class="row">
-                <div class="left-image col-lg-5 col-md-12 col-sm-12 mobile-bottom-fix-big"
-                    data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                <div class="left-image col-lg-5 col-md-12 col-sm-12 mobile-bottom-fix-big" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
                     <img src="plugins/template-landingpage/assets/images/left-image.png" class="rounded img-fluid d-block mx-auto" alt="App">
                 </div>
                 <div class="right-text offset-lg-1 col-lg-6 col-md-12 col-sm-12 mobile-bottom-fix">
@@ -175,17 +170,15 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum natus neque
                             <img src="plugins/template-landingpage/assets/images/about-icon-01.png" alt="">
                             <div class="text">
                                 <h4>Vestibulum pulvinar rhoncus</h4>
-                                <p>Please do not redistribute this template ZIP file for a download purpose. You may <a
-                                rel="nofollow" href="https://templatemo.com/contact" target="_parent">contact</a> us for
-                            additional licensing of our template or to get a PSD file.</p>
+                                <p>Please do not redistribute this template ZIP file for a download purpose. You may <a rel="nofollow" href="https://templatemo.com/contact" target="_parent">contact</a> us for
+                                    additional licensing of our template or to get a PSD file.</p>
                             </div>
                         </li>
                         <li data-scroll-reveal="enter right move 30px over 0.6s after 0.5s">
                             <img src="plugins/template-landingpage/assets/images/about-icon-02.png" alt="">
                             <div class="text">
                                 <h4>Sed blandit quam in velit</h4>
-                                <p>You can <a rel="nofollow"
-                                        href="https://templatemo.com/tm-540-lava-landing-page">download Lava
+                                <p>You can <a rel="nofollow" href="https://templatemo.com/tm-540-lava-landing-page">download Lava
                                         Template</a> from our website. Duis viverra, ipsum et scelerisque placerat, orci
                                     magna consequat ligula.</p>
                             </div>
@@ -218,8 +211,7 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum natus neque
                             Donec vitae magna ornare, pellentesque ex vitae, aliquet urna.</p>
                     </div>
                 </div>
-                <div class="col-lg-10 col-md-12 col-sm-12 mobile-bottom-fix-big"
-                    data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                <div class="col-lg-10 col-md-12 col-sm-12 mobile-bottom-fix-big" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
                     <div class="owl-carousel owl-theme">
                         <div class="item service-item">
                             <div class="author">
@@ -310,20 +302,17 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum natus neque
                                 <div class="row">
                                     <div class="col-md-6 col-sm-12">
                                         <fieldset>
-                                            <input name="name" type="text" id="name" placeholder="Full Name" required=""
-                                                style="background-color: rgba(250,250,250,0.3);">
+                                            <input name="name" type="text" id="name" placeholder="Full Name" required="" style="background-color: rgba(250,250,250,0.3);">
                                         </fieldset>
                                     </div>
                                     <div class="col-md-6 col-sm-12">
                                         <fieldset>
-                                            <input name="email" type="text" id="email" placeholder="E-Mail Address"
-                                                required="" style="background-color: rgba(250,250,250,0.3);">
+                                            <input name="email" type="text" id="email" placeholder="E-Mail Address" required="" style="background-color: rgba(250,250,250,0.3);">
                                         </fieldset>
                                     </div>
                                     <div class="col-lg-12">
                                         <fieldset>
-                                            <textarea name="message" rows="6" id="message" placeholder="Your Message"
-                                                required="" style="background-color: rgba(250,250,250,0.3);"></textarea>
+                                            <textarea name="message" rows="6" id="message" placeholder="Your Message" required="" style="background-color: rgba(250,250,250,0.3);"></textarea>
                                         </fieldset>
                                     </div>
                                     <div class="col-lg-12">
@@ -341,9 +330,9 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum natus neque
                         <h2>More About <em>Lava</em></h2>
                         <p>Phasellus dapibus urna vel lacus accumsan, iaculis eleifend leo auctor. Duis at finibus odio.
                             Vivamus ut pharetra arcu, in porta metus. Suspendisse blandit pulvinar ligula ut elementum.
-                            <br><br>If you need this contact form to send email to your inbox, you may follow our <a
-                                rel="nofollow" href="https://templatemo.com/contact" target="_parent">contact</a> page
-                            for more detail.</p>
+                            <br><br>If you need this contact form to send email to your inbox, you may follow our <a rel="nofollow" href="https://templatemo.com/contact" target="_parent">contact</a> page
+                            for more detail.
+                        </p>
                         <ul class="social">
                             <li><a href="https://fb.com/templatemo"><i class="fa fa-facebook"></i></a></li>
                             <li><a href="#"><i class="fa fa-twitter"></i></a></li>
@@ -359,7 +348,7 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum natus neque
                     <div class="sub-footer">
                         <p>Copyright &copy; 2020 Lava Landing Page
 
-                        | Designed by <a rel="nofollow" href="https://templatemo.com">TemplateMo</a></p>
+                            | Designed by <a rel="nofollow" href="https://templatemo.com">TemplateMo</a></p>
                     </div>
                 </div>
             </div>
@@ -384,4 +373,5 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum natus neque
     <script src="plugins/template-landingpage/assets/js/custom.js"></script>
 
 </body>
+
 </html>
